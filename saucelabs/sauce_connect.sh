323da1bb69c5c6e4bf396efec7e613af5bb31fe5
Gemma Anible <gemma@wonderproxy.com>
2018-01-18 13:19:21 +0100

    lint the shell scripts

--- saucelabs/sauce_connect.sh
+++ saucelabs/sauce_connect.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 # check for servers
-if [ -z "$@" ]; then
+if [[ -z "$@" ]]; then
     echo "You need to specify one or more WonderProxy server names"
     exit 4
 fi
 
 # check for sauce creds
-if [ -z "$SAUCE_USERNAME" ] || [ -z "$SAUCE_ACCESS_KEY" ]; then
+if [[ -z "$SAUCE_USERNAME" || -z "$SAUCE_ACCESS_KEY" ]]; then
     echo "You need to set SAUCE_USERNAME and SAUCE_ACCESS_KEY"
     exit 1
 fi
 
 # check for wonderproxy creds
-if [ -z "$WONDERPROXY_USER" ] || [ -z "$WONDERPROXY_PASS" ]; then
+if [[ -z "$WONDERPROXY_USER" || -z "$WONDERPROXY_PASS" ]]; then
     echo "You need to set WONDERPROXY_USER and WONDERPROXY_PASS"
     exit 2
 fi
 
 # check for the sc binary
-if [ -z `which sc` ]; then
+if [[ -z $(which sc) ]]; then
     echo "Can't find the Sauce Connect binary (sc), make sure it's in your PATH"
     exit 3
 fi
@@ -32,47 +34,47 @@ seport=4445  # --se-port
 
 # set up the temp directory
 saucedir="/tmp/sauce-connect"
-mkdir -p $saucedir
+mkdir -p "$saucedir"
 
 # set up one sauce tunnel for each server passed on the command line
 for server; do
     printf "\n"
-    pidfile=$saucedir/$server.pid
+    pidfile="$saucedir/$server.pid"
 
     # clean up old tunnels
-    if [ -f "$pidfile" ]; then
+    if [[ -f "$pidfile" ]]; then
         echo You already have a tunnel running for $server. \
-            The PID is `cat "$pidfile"`.
+            The PID is "$(cat "$pidfile")".
         continue
     fi
-    rm -f $saucedir/$server.*
+    rm -f "$saucedir/$server".*
 
     echo Starting a Sauce Connect tunnel for $server in the background. Check \
-        $saucedir/$server.* for logs and output.
+        "$saucedir/$server".* for logs and output.
 
     sc \
-       -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY \
-       -p $server.wonderproxy.com:11000 \
-       -w $WONDERPROXY_USER:$WONDERPROXY_PASS \
-       -T -l $saucedir/$server.log -i $server \
-       --pidfile $pidfile \
-       --scproxy-port $scport -P $seport \
-       > $saucedir/$server.output.log 2>&1 &
+       -u "$SAUCE_USERNAME" -k "$SAUCE_ACCESS_KEY" \
+       -p "$server.wonderproxy.com:11000" \
+       -w "$WONDERPROXY_USER:$WONDERPROXY_PASS" \
+       -T -l "$saucedir/$server.log" -i "$server" \
+       --pidfile "$pidfile" \
+       --scproxy-port "$scport" -P "$seport" \
+       > "$saucedir/$server.output.log" 2>&1 &
     
     seconds=0
-    while [ "$seconds" -lt 50 -a ! -f "$pidfile" ]; do
-        seconds=$[$seconds +1]
+    while [[ "$seconds" -lt 50 && ! -f "$pidfile" ]]; do
+        let "seconds += 1"
         sleep .1
     done
 
-    if [ -f "$pidfile" ]; then
-        echo When you are ready to stop the $server tunnel, kill `cat "$pidfile"`
+    if [[ -f "$pidfile" ]]; then
+        echo When you are ready to stop the $server tunnel, kill "$(cat "$pidfile")"
     else
         echo WARNING: The PID file for $server was not created, check the logs
     fi
 
-    scport=$[$scport +1]
-    seport=$[$seport +1]
+    let "scport += 1"
+    let "seport += 1"
 done
 
 printf "\n"
@@ -82,15 +84,15 @@ echo Tunnels are spinning up, this may take a few seconds...
 seconds=0
 servers_done=""
 in_progress=true
-while [ "$seconds" -lt 75 ] && [ $in_progress ]; do
+while [[ "$seconds" -lt 75 && $in_progress ]]; do
     in_progress=
-    seconds=$[$seconds +1]
+    let "seconds += 1"
     printf "."
 
     for server; do
-        log=$saucedir/$server.output.log
-        if ! [[ "$servers_done" =~ " $server " ]]; then
-            if [ -f "$log" ] && [ "`grep 'Sauce Connect is up' $log`" ]; then
+        log="$saucedir/$server.output.log"
+        if [[ ! "$servers_done" =~ [[:space:]]$server[[:space:]] ]]; then
+            if grep -q 'Sauce Connect is up' "$log"; then
                 servers_done="$servers_done $server "
             else
                 in_progress=true
@@ -103,7 +105,7 @@ done
 
 printf "\n"
 for server; do
-    if [[ "$servers_done" =~ " $server " ]]; then
+    if [[ "$servers_done" =~ [[:space:]]$server[[:space:]] ]]; then
         echo $server is ready
     else
         echo WARNING: Sauce Connect tunnel for $server is not ready, check the logs
--- saucelabs/sauce_disconnect.sh
+++ saucelabs/sauce_disconnect.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 saucedir="/tmp/sauce-connect"
 
 # set up one sauce tunnel for each server passed on the command line
 for server; do
-    pidfile=$saucedir/$server.pid
+    pidfile="$saucedir/$server.pid"
 
-    if [ -f "$pidfile" ]; then
-        pid=`cat "$pidfile"`
-        echo Killing Sauce Connect tunnel for $server, PID $pid
+    if [[ -f "$pidfile" ]]; then
+        pid="$(cat "$pidfile")"
+        echo "Killing Sauce Connect tunnel for $server, PID $pid"
 
-        kill $pid
-        while [ -e /proc/$pid ]; do
+        kill "$pid"
+        while [ -e "/proc/$pid" ]; do
             sleep 0.1
         done
 
-        rm $saucedir/$server.*
     else
-        echo $pidfile does not exist, skipping
-        continue
+        echo "No process ID for $server"
     fi
+
+    rm -f "$saucedir/$server".*
 done
