4423131f57141758cadc9da6d4db33192ad9e95b
Gemma Anible <gemma@wonderproxy.com>
2021-09-23 10:22:35 +0200

    run the same tests as the other languages/platforms

--- playwright/readme.md
+++ playwright/readme.md
@@ -1,46 +1,71 @@
-# Getting started with Playwright and Wonder Proxy
+These tests use [Playwright](https://playwright.dev) and
+[WonderProxy](https://wonderproxy.com) to make sure a location-aware
+[WonderNetwork website](https://wondernetwork.com/geotest) looks the way it's
+supposed to from a few different places around the world.
 
-* Playwright needs Node v12 or higher (see package.json for reference)
-* `npm init`
-   * set project name, etc, skip if adding test to existing project
-   * when it asks the command to run the tests: `npx playwright test`
-* `npx gitignore node`
-   * this adds a reasonable default .gitignore file
-* `npm i -D @playwright/test`
-  * install playwright into node_modules and save it as a development dependency 
-* `npx playwright install`
-  * this will download browsers to work with (240mb or so)
-  * also sets up some playwright helpers
-* write tests
-  * create directory: `test`
-  * create file: `WonderProxy.spec.js`
-* run tests
-  * `npm test` (which runs `npx playwright test`)
-  * Alternatives:
-    * `npx playwright test --headed`
-    * `npx playwright test --browser=all`
-    * `npx playwright test --browser=firefox`
-    * See: https://playwright.dev/docs/intro#learning-the-command-line
-  * With proxy auth
-    * `PROXY_USER=<username> PROXY_PASS=<secretPassword> npm test`
-    * `PROXY_USER=<username> PROXY_PASS=<secretPassword> npx playwright test --headed`
-* More docs:
-  * Playwright:
-    * https://playwright.dev/docs/intro
-    * https://playwright.dev/docs/test-assertions
-    * https://playwright.dev/docs/test-fixtures
-    * https://playwright.dev/docs/test-configuration
-    * https://playwright.dev/docs/intro#learning-the-command-line
-  * CSS Selectors:
-    * https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors
-    * https://www.checklyhq.com/learn/headless/basics-selectors/
-    * https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
-* Integrating Wonder Proxy
-  * Proxy servers must be added in the WP portal before running the test
-  * Setting proxy and authentication:
-    * Add proxy configuration to the test fixture in a test.describe block
-    * See the code for a sample of this.
-    * Username and password are taken from the environment, set these in the environment before running the test
-      * This is to keep them from being checked in to source control.
-      * when setting credentials in IDE configuration make sure that is not being checked into source control.
-  * Note that Venice and Amsterdam have cookie acceptance dialogs that obscure the buttons, so while the buttons are there they are not visible to a human user until the dialog is accepted.
+## Setup
+
+1. Playwright needs NodeJS v12 or higher (see `package.json` for reference).
+2. If you're setting up a new project instead of using our
+   pre-built `package.json`:
+   1. Initialize `npm`:
+      ```
+      $ npm init
+      ```
+      The command to run tests should be `npx playwright test`
+   2. Create a reasonable `.gitignore` file:
+      ```
+      $ npx gitignore node
+      ```
+   3. Add Playwright Test as a dev dependency:
+      ```
+      $ npm install --save-dev @playwright/test
+      ```
+3. Install the dependencies:
+   ```
+   $ npm install
+   $ npx playwright install
+   ```
+4. The tests will pull your WonderProxy credentials from environment variables:
+   ```
+   $ export WONDERPROXY_USER=<your WonderProxy username>
+   $ export WONDERPROXY_PASS=<your WonderProxy password>
+   ```
+5. Our demo tests use the **Albquerque**, **Toronto**, and **Vancouver** WonderProxy
+   locations, so make sure
+   you've [added those servers to your account](https://wonderproxy.com/my/servers).
+
+## Write your tests
+
+Playwright will automically run any test file in the `./test` directory that
+ends with `.spec.js`. In this demo, that's `./test/WonderProxy.spec.js`.
+
+## Run your tests
+
+This Playwright demo has been tested on with Playwright 1.14 and 1.15, running
+on Node 12 and 14.
+
+The default `npm test` command is configured to run `npx playwright test`, so
+you can use that to run your tests:
+
+```
+$ npm test
+```
+
+Additional settings are available on
+the [command-line](https://playwright.dev/docs/intro#learning-the-command-line):
+
+```
+$ npx playwright test --headed          # run the browser with a graphical interface
+$ npx playwright test --browser=all     # run tests in all the installed browsers
+$ npx playwright test --browser=firefox # run tests in firefox
+```
+
+## Resources
+
+* https://playwright.dev/docs/intro
+* https://playwright.dev/docs/test-assertions
+* https://playwright.dev/docs/test-fixtures
+* https://playwright.dev/docs/test-configuration
+* https://playwright.dev/docs/intro#learning-the-command-line
+* https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors
--- playwright/test/WonderProxy.spec.js
+++ playwright/test/WonderProxy.spec.js
@@ -1,91 +1,34 @@
 const { test, expect } = require('@playwright/test');
 
-
-// Playwright basics
-test('Wonder Proxy home page title', async ({ page }) => {
-  await page.goto('https://wonderproxy.com');
-  expect(await page.title()).toBe('Localization testing with confidence - WonderProxy');
-});
-
-test('Wonder Proxy status page title', async ({ page }) => {
-  await page.goto('https://wonderproxy.com/servers/status');
-  expect(await page.title()).toBe('Server Status - WonderProxy');
-});
-
-// Check server status:
-const serverNames = ['Lansing', 'Nuremberg', 'Koto'];
-for (const serverName of serverNames) {
-  test(`Server status for ${serverName} is "up"`, async ({page}) => {
-    await page.goto('https://wonderproxy.com/servers/status');
-
-    // wait for the list to be selectable
-    const statusListSelector = '#status > div.has-shadow > ul > li';
-    await page.waitForSelector(statusListSelector);
-
-    // evaluate list of server status and turn it into an array of name and status
-    const statusList = await page.evaluate((selector) => {
-      // This will not crash if the queried element is not found on the page:
-      const safeQueryText = (element, query) => {
-        const result = element.querySelector(query);
-        return result ? result.textContent : 'unable to locate element';
-      }
-      // This is evaluated in the context of the browser which allows cool tricks but can be confusing.
-      const statusElements = Array.from(document.querySelectorAll(selector));
-      return statusElements.map(statusElement => {
-        const name = safeQueryText(statusElement, 'span.title.is-block > a');
-        const status = safeQueryText(statusElement,'span.server-status.is-up');
-        return {name, status};
-      });
-    }, statusListSelector);
-
-    const serverStatus = statusList.find(s => s.name === serverName);
-    const status = serverStatus ? serverStatus.status : 'unknown';
-    expect(status).toBe('up');
-  });
-}
-
-
-// Wonder Proxy and Puppeteer integration: Google.com world tour
 const locations = [
-  { location: 'Lansing', server: 'lansing.wonderproxy.com', searchButtonText: 'Google Search', luckyButtonText: 'I\'m Feeling Lucky' },
-  { location: 'Amsterdam', server: 'amsterdam.wonderproxy.com', searchButtonText: 'Google zoeken', luckyButtonText: 'Ik doe een gok' },
-  { location: 'Venice', server: 'venice.wonderproxy.com', searchButtonText: 'Cerca con Google', luckyButtonText: 'Mi sento fortunato' },
-  { location: 'Tokyo', server: 'tokyo.wonderproxy.com', searchButtonText: 'Google 検索', luckyButtonText: 'I\'m Feeling Lucky' },
+    { title: 'Albuquerque', server: 'albuquerque.wonderproxy.com:11000' },
+    { title: 'Toronto', server: 'toronto.wonderproxy.com:11000' },
+    { title: 'Vancouver', server: 'vancouver.wonderproxy.com:11000' },
 ];
 
 for (const location of locations) {
-  // Use "test.describe" to isolate "test.use" fixture setups
-  test.describe(`Google.com should have location specific information for ${location.location}`, () => {
+    test.describe(`The WonderNetwork Geotest page from ${location.server}`, () => {
+        // use the proxy for each test
+        test.use({
+            proxy: {
+                server: `http://${location.server}`,
+                username: process.env.PROXY_USER,
+                password: process.env.PROXY_PASS
+            }
+        });
 
-    // Modify fixture setup with "test.use"
-    test.use({
-      proxy: {
-        server: `http://${location.server}:10000`,
-        username: process.env.PROXY_USER,
-        password: process.env.PROXY_PASS
-      }
+        test.beforeEach(async ({ page }) => {
+            await page.goto('https://wondernetwork.com/geotest');
+        });
+
+        test('has the right title', async ({page}) => {
+            await expect(page).toHaveTitle('Geotest - WonderNetwork');
+        });
+
+        test(`displays ${location.title} as the city`, async ({page}) => {
+            const cityLocator = await page.locator('#user-city');
+
+            await expect(cityLocator).toHaveText(location.title);
+        });
     });
-
-    test(`button text`, async ({page}) => {
-      await page.goto('https://google.com');
-
-      // wait for the list to be selectable
-      const inputSelector = 'input[type="submit"]';
-      await page.waitForSelector(inputSelector, {state: "attached"});
-
-      const inputList = await page.evaluate((selector) => {
-        // This is evaluated in the context of the browser which allows cool tricks but can be confusing.
-        const elements = Array.from(document.querySelectorAll(selector));
-        return elements.map(element => element.defaultValue);
-      }, inputSelector);
-
-      // This will get better failure messages indicating expected and actual results
-      expect(inputList[0]).toBe(location.searchButtonText);
-      expect(inputList[1]).toBe(location.luckyButtonText);
-
-      // This is more robust, will not break if the buttons are re-arranged.
-      expect(inputList).toContain(location.searchButtonText);
-      expect(inputList).toContain(location.luckyButtonText);
-    });
-  });
 }
